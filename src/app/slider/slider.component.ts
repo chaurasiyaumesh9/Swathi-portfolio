46da3dce02fedd8e12e59009279bd2d9a9877802
AdaptyIndia <umesh.chaurasiya@adapty.com>
2018-04-03 00:51:43 +0530

    added slider component

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
+	Data:Array<any> = [{
+		Title: "Image 1",
+		URL: "assets/images/img_fjords_wide.jpg"
+	},{
+		Title: "Image 2",
+		URL: "assets/images/img_lights_wide.jpg"
+	},{
+		Title: "Image 3",
+		URL: "assets/images/img_mountains_wide.jpg"
+	},{
+		Title: "Image 4",
+		URL: "assets/images/img_nature_wide.jpg"
+	}];
+	CurrentIndex:Number = 0;
+	SlideWidth:Number = 1000;
+	MoveBy:any;
+	NumSlides:Number = this.Data.length;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  GoBack( event ){
+  	event.preventDefault();
+  	if ( this.CurrentIndex >0 ) { this.CurrentIndex--};
+  	let MoveBy = this.SlideWidth * this.CurrentIndex;
+  	this.MoveBy = "translateX(-"+ MoveBy +"px)";
+  }
+  MoveNext( event ){
+  	event.preventDefault();
+  	if ( this.CurrentIndex < this.NumSlides-1) { this.CurrentIndex++};
+  	let MoveBy = this.SlideWidth * this.CurrentIndex;
+  	this.MoveBy = "translateX(-"+ MoveBy +"px)";
+  }
+  MoveTo( _index, _event ){
+  	_event.preventDefault();
+  	this.CurrentIndex = _index || 0;
+  	let MoveBy = this.SlideWidth * this.CurrentIndex;
+  	this.MoveBy = "translateX(-"+ MoveBy +"px)";
   }
 
 }
